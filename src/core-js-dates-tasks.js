19815a8c2ebc7d0ef410d5fcbf3131218d7234b8
AndriiHordov <146883659+AndriiHordov@users.noreply.github.com>
2024-03-10 10:13:06 +0100

    feat: implement the isDateInPeriod function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -116,8 +116,13 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  let now = date;
+  let { start, end } = period;
+  [now, start, end] = [now, start, end].map((dateString) =>
+    Date.parse(dateString)
+  );
+  return now >= start && now <= end;
 }
 
 /**
